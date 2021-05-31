3ae56500d83d66884a639660a598b4a23f79a4a8
simonlei <simonlei@tencent.com>
2021-05-31 09:45:53 +0800

    new deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 git pull
-mvn package -DskipTests
+mvn clean package -DskipTests
 mv mpm-server/target/mpm-server-1.0-SNAPSHOT.war ../jetty9/webapps/mpm.war
 # stop jetty
 cd ../jetty9/
 java -DSTOP.PORT=27077 -DSTOP.KEY=stop123 -jar start.jar --stop
-cd ../jetty9/webapps/
+cd webapps/
 mkdir -p bak
-mv ROOT ~/bak/ROOT_bak.`date "+%Y.%m.%d"`
+mv ROOT bak/ROOT_bak.`date "+%Y.%m.%d"`
 mkdir ROOT
 mv mpm.war ROOT
 cd ROOT
 jar xf mpm.war
+# cd to jetty9
 cd ../..
-cp ~/config/application.properties webapps/ROOT/WEB-INF/classes/
+cp ../config/application.properties webapps/ROOT/WEB-INF/classes/
 # start jetty
 nohup java -Djetty.http.port=27277 -DSTOP.PORT=27077 -DSTOP.KEY=stop123 -jar start.jar  > nohup.log &
