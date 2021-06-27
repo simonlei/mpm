f6d1476cd7678d8504edf6660803ba41006d2bde
simonlei <simonlei@tencent.com>
2021-06-27 23:19:17 +0800

    这样就可以知道key的情况了

--- app/lib/pics_grid.dart
+++ app/lib/pics_grid.dart
@@ -2,6 +2,7 @@ import 'package:app/image_tile.dart';
 import 'package:app/pics_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:logger/logger.dart';
 
 class PicsGrid extends StatefulWidget {
   @override
@@ -21,24 +22,34 @@ class _PicsGridState extends State<PicsGrid> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<PicImage?>(
-      future: _picsModel.getImage(0),
-      builder: (BuildContext context, AsyncSnapshot<PicImage?> snapshot) {
-        if (snapshot.hasData) {
-          return StaggeredGridView.extentBuilder(
-            maxCrossAxisExtent: 200,
-            crossAxisSpacing: 5,
-            mainAxisSpacing: 3,
-            itemCount: _picsModel.getTotalImages(),
-            itemBuilder: (context, index) =>
-                ImageTile(index, _picsModel, _selectImage),
-            staggeredTileBuilder: (index) => const StaggeredTile.extent(1, 150),
-          );
-        } else if (snapshot.hasError)
-          return Text('Error:${snapshot.error}');
-        else
-          return CircularProgressIndicator();
-      },
+    return Focus(
+      autofocus: true,
+      onKey: onKey,
+      child: FutureBuilder<PicImage?>(
+        future: _picsModel.getImage(0),
+        builder: (BuildContext context, AsyncSnapshot<PicImage?> snapshot) {
+          if (snapshot.hasData) {
+            return StaggeredGridView.extentBuilder(
+              maxCrossAxisExtent: 200,
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 3,
+              itemCount: _picsModel.getTotalImages(),
+              itemBuilder: (context, index) =>
+                  ImageTile(index, _picsModel, _selectImage),
+              staggeredTileBuilder: (index) =>
+                  const StaggeredTile.extent(1, 150),
+            );
+          } else if (snapshot.hasError)
+            return Text('Error:${snapshot.error}');
+          else
+            return CircularProgressIndicator();
+        },
+      ),
     );
+  }
+
+  onKey(FocusNode node, RawKeyEvent event) {
+    Logger().i("ctrl key down ${event.isControlPressed} ");
+    return true;
   }
 }
