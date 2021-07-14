d7ee30f7586a379faeee4d5d90d0267ec8284213
simonlei <simonlei@qq.com>
2021-07-14 18:49:06 +0800

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 git pull
+cd app
+# install flutter first https://snapcraft.io/install/flutter/centos, after install, flutter at /snap/bin
+flutter build web --dart-define=remote_addr=
+cd ..
 mvn clean package -DskipTests
 mv mpm-server/target/mpm-server-1.0-SNAPSHOT.war ../jetty9/webapps/mpm.war
 # stop jetty
@@ -11,6 +15,7 @@ mkdir ROOT
 mv mpm.war ROOT
 cd ROOT
 jar xf mpm.war
+cp -R ../../../mpm/app/build/web/* .
 # cd to jetty9
 cd ../..
 cp ../config/application.properties webapps/ROOT/WEB-INF/classes/
