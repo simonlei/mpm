b369b5c5074fadb5bc3d83937cd14355d29fbde7
simonlei <simonlei@qq.com>
2023-10-28 00:07:34 +0800

    feat: 简化登陆页面

--- mpm-vue3/src/pages/login/components/Login.vue
+++ mpm-vue3/src/pages/login/components/Login.vue
@@ -39,42 +39,10 @@
       </div>
     </template>
 
-    <!-- 扫码登陆 -->
-    <template v-else-if="type == 'qrcode'">
-      <div class="tip-container">
-        <span class="tip">请使用微信扫一扫登录</span>
-        <span class="refresh">刷新 <t-icon name="refresh" /> </span>
-      </div>
-      <qrcode-vue value="" :size="192" level="H" />
-    </template>
-
-    <!-- 手机号登陆 -->
-    <template v-else>
-      <t-form-item name="phone">
-        <t-input v-model="formData.phone" size="large" placeholder="请输入手机号码">
-          <template #prefix-icon>
-            <t-icon name="mobile" />
-          </template>
-        </t-input>
-      </t-form-item>
-
-      <t-form-item class="verification-code" name="verifyCode">
-        <t-input v-model="formData.verifyCode" size="large" placeholder="请输入验证码" />
-        <t-button variant="outline" :disabled="countDown > 0" @click="sendCode">
-          {{ countDown == 0 ? '发送验证码' : `${countDown}秒后可重发` }}
-        </t-button>
-      </t-form-item>
-    </template>
 
     <t-form-item v-if="type !== 'qrcode'" class="btn-container">
       <t-button block size="large" type="submit"> 登录 </t-button>
     </t-form-item>
-
-    <div class="switch-container">
-      <span v-if="type !== 'password'" class="tip" @click="switchType('password')">使用账号密码登录</span>
-      <span v-if="type !== 'qrcode'" class="tip" @click="switchType('qrcode')">使用微信扫码登录</span>
-      <span v-if="type !== 'phone'" class="tip" @click="switchType('phone')">使用手机号登录</span>
-    </div>
   </t-form>
 </template>
 
--- mpm-vue3/src/pages/login/index.vue
+++ mpm-vue3/src/pages/login/index.vue
@@ -1,21 +1,15 @@
 <template>
   <div class="login-wrapper">
-    <login-header />
 
     <div class="login-container">
       <div class="title-container">
         <h1 class="title margin-no">登录到</h1>
-        <h1 class="title">TDesign Starter</h1>
+        <h1 class="title">My Photo Manager</h1>
         <div class="sub-title">
-          <p class="tip">{{ type == 'register' ? '已有账号?' : '没有账号吗?' }}</p>
-          <p class="tip" @click="switchType(type == 'register' ? 'login' : 'register')">
-            {{ type == 'register' ? '登录' : '注册新账号' }}
-          </p>
         </div>
       </div>
 
       <login v-if="type === 'login'" />
-      <register v-else @register-success="switchType('login')" />
       <tdesign-setting />
     </div>
 
@@ -31,8 +25,6 @@ export default {
 import { ref } from 'vue';
 
 import Login from './components/Login.vue';
-import Register from './components/Register.vue';
-import LoginHeader from './components/Header.vue';
 import TdesignSetting from '@/layouts/setting.vue';
 
 const type = ref('login');
