58ba392114f38ae82c71e7fbeac48f1a237be3f0
simonlei <simonlei@qq.com>
2023-12-17 21:28:50 +0800

    feat: 增加确认

--- mpm-vue3/src/pages/dashboard/photos/components/FolderTreeContextMenu.vue
+++ mpm-vue3/src/pages/dashboard/photos/components/FolderTreeContextMenu.vue
@@ -1,19 +1,32 @@
 <script lang="ts" setup>
 import {switchTrashFolder} from "@/api/photos";
 import {photoFilterStore} from "@/store";
+import {DialogPlugin, MessagePlugin} from "tdesign-vue-next";
 
 const props = defineProps({node: null});
 defineEmits(['update:node']);
 const filterStore = photoFilterStore();
 
 async function deleteSelectedFolder(value: string) {
-  // TODO: confirm
-  const result = await switchTrashFolder(!filterStore.trashed, value);
-  console.log("delete {} result {}", value, result);
-  if (result > 0) {
-    filterStore.change({path: ''});
-    // TREE_DATA.value = await getFolderTreeWithRoot();
-  }
+  const confirmDia = DialogPlugin({
+    header: filterStore.trashed ? '确认恢复目录？' : '确认删除目录？',
+    body: filterStore.trashed ? '将目录下所有照片恢复？' : '将目录下所有照片移入回收站？',
+    confirmBtn: '确认',
+    cancelBtn: '取消',
+    onConfirm: async ({e}) => {
+      const result = await switchTrashFolder(!filterStore.trashed, value);
+      console.log("delete {} result {}", value, result);
+      if (result > 0) {
+        filterStore.change({path: ''});
+        MessagePlugin.success(`已处理照片${result}`);
+      }
+      confirmDia.hide();
+    },
+    onClose: ({e, trigger}) => {
+      confirmDia.hide();
+    },
+  });
+  confirmDia.show();
 }
 
 </script>
