2e445124129cf31461fd73e35d4a4345feaa4e78
simonlei <simonlei@qq.com>
2023-12-17 23:18:27 +0800

    wip: 对话框不知道为啥没展示出来...

--- mpm-vue3/src/pages/dashboard/photos/components/PhotoItemContextMenu.vue
+++ mpm-vue3/src/pages/dashboard/photos/components/PhotoItemContextMenu.vue
@@ -1,5 +1,22 @@
 <script lang="ts" setup>
 
+import {selectModuleStore} from "@/store/modules/select-module";
+import {MessagePlugin} from "tdesign-vue-next";
+import {ref} from "vue";
+
+const selectModule = selectModuleStore();
+const dateSelectDialog = ref(null);
+let dateSelectDialogVisible = ref(false);
+
+function changePhotoDate() {
+  if (selectModule.lastSelectedIndex == -1) {
+    MessagePlugin.warning('请选中照片后再试');
+    return;
+  }
+  //dateSelectDialog.show();
+  dateSelectDialogVisible.value = true;
+  console.log("dialog " + dateSelectDialog);
+}
 
 </script>
 
@@ -7,7 +24,7 @@
   <t-dropdown :max-column-width="250" :show-arror="true" placement="right" trigger="context-menu">
     <slot></slot>
     <t-dropdown-menu>
-      <t-dropdown-item content="修改选中照片的时间"></t-dropdown-item>
+      <t-dropdown-item content="修改选中照片的时间" @click="changePhotoDate"></t-dropdown-item>
       <t-dropdown-item content="修改选中照片的描述信息"></t-dropdown-item>
       <t-dropdown-item content="拷贝选中照片的GIS信息"></t-dropdown-item>
       <t-dropdown-item content="修改选中照片的GIS信息"></t-dropdown-item>
@@ -17,6 +34,14 @@
       -->
     </t-dropdown-menu>
   </t-dropdown>
+
+  <t-dialog ref="dateSelectDialog" v-model:visible="dateSelectDialogVisible" header="请选择日期"
+            height="300px"
+            width="300px">
+    hello...
+    <t-button>xzxx</t-button>
+    <t-date-picker allowInput/>
+  </t-dialog>
 </template>
 
 <style lang="less" scoped>
